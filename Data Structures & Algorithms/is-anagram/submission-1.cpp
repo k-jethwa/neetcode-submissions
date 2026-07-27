5c6013e07fa7a8a2bb23912e7d4655c384a96da4
Kaavya Jethwa <kjethwatemp@gmail.com>
2026-07-27 15:50:40 -0500

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> s_chars;
+        unordered_map<char, int> t_chars;
+
+        for (char c: s) {
+            s_chars[c]++;
+        }
+        for (char c: t) {
+            t_chars[c]++;
+        }
+
+        if (s_chars != t_chars) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+};
