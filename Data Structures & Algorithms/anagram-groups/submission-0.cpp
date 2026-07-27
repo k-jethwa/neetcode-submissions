2226c8f4524f1730a687e87147ae529e6bdb9ee0
Kaavya Jethwa <kjethwatemp@gmail.com>
2026-07-27 15:58:45 -0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        unordered_map<string, vector<string>> groups;
+
+        for (string s: strs) {
+            string sorted = s;
+            sort(sorted.begin(), sorted.end());
+            groups[sorted].push_back(s);
+        }
+
+        vector<vector<string>> res;
+
+        for (auto& key: groups) {
+            res.push_back(key.second);
+        }
+
+        return res;
+        
+    }
+};
